81e565853abb379f367973a89c2496e9fc38c998
Dave Fredkove <spot4dave@gmail.com>
2020-09-15 11:53:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
   console.log('New connection')
-  res.end('Hello there gcbrun2! :) ')
+  res.end('Hello there gcbrun2! :)  ::ninja edit:: ')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
